ddcf851aced5ce253e233353d53a760a671607e9
Jeremia Kimelman <jbkimelman@gmail.com>
2014-09-03 21:08:23 -0400

    put plan-env in config obj

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || 'I Am A Monkey',
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
-  stripeSecret: process.env.STRIPE_SECRET
+  stripeSecret: process.env.STRIPE_SECRET,
+  stripePlan: process.env.STRIPE_PLAN
 };
