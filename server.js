f3957510772eb163150d85b06ffa907a2e980d7e
Jeremia Kimelman <jbkimelman@gmail.com>
2017-04-02 02:24:26 -0400

    Add donation handler endpoints
    Move a bunch of files around, with the hopeful end result of making it
    easier to understand. Instead of using the Stripe API library, I just
    wrapped a bunch of API calls in helper functions that return promises.
    
    We want people to be able to give any amount (USD) as a one time
    donation or on a monthly basis. So the donate endpoint should handle
    both of those cases instead of the previous way of using two different
    endpoints.
    
    The endpoint needs an amount (USD), an email, an indication of if the
    donation is recurring and a token from Stripe. It should pass along the
    successful API response or some sort of error for the client to handle.

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express')
+var cors = require('cors')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+//var csrf = require('lusca').csrf()
+var config = require('./config')
+
+var server = express()
+
+server.disable('x-powered-by')
+server.set('host', config.host)
+server.set('port', config.port)
+server.set('env', config.env)
+server.set('startTime', new Date())
+server.use(logger('dev'))
+server.use(cors())
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(cookieParser())
+server.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: true,
+  resave: true,
+}))
+
+server.use(function(req, res, next) {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: {}
+  })
+})
+
+module.exports = server
